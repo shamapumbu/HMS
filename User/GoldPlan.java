055f7e5fd072905bd186ed4ff64e3e398589a1d3
shamapumbu <88844339+shamapumbu@users.noreply.github.com>
2022-04-06 16:00:45 +0200

    Update GoldPlan.java

--- User/GoldPlan.java
+++ User/GoldPlan.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class GoldPlan extends HealthInsurancePlan {
-    public GoldPlan(double cov) {
-        super.setCoverage(cov);
+    public GoldPlan() {
+        super.setCoverage(0.8);
     }
 }
